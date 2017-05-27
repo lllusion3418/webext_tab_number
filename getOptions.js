7feebc493dbafd4a4b668c130c429437f02b5d49
lllusion3418 <lllusion3418@gmail.com>
2017-05-28 00:49:42 +0200

    icon_sizes comment

--- getOptions.js
+++ getOptions.js
@@ -10,7 +10,7 @@ var defaultOptions = {
     "badgeBg": "#212121",
 
     // if displayMode == "icon"
-    "iconDimension": 64,
+    "iconDimension": 64, // refer https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json/browser_action#Choosing_icon_sizes
     "iconFont": "sans-serif",
     "iconColor": "#000000"
 };
